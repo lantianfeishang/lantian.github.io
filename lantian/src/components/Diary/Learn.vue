81a07eeca507963b8a47625040d9774f3f3c8725
lantianfeishang <2983911814@qq.com>
2024-10-16 22:50:26 +0800

    blog

--- lantian/src/components/Diary/Learn.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <el-row>
-    <el-col :span="24"> </el-col>
-  </el-row>
-</template>
-
-<script lang="ts" setup name="Home"></script>
-
-<style scoped></style>
--- lantian/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import router from './router/router'
-import App from './App.vue'
-
-const app = createApp(App)
-app.use(ElementPlus)
-app.use(router).mount('#app')
